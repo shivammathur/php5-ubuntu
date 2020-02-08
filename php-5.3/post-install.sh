6026e7a3f5ede7280fd3689ceb26abe2842fdcf8
Shivam Mathur <shivam_jpr@hotmail.com>
2020-02-09 04:50:36 +0530

    Fix post-install.sh to add tz

--- php-5.3/post-install.sh
+++ php-5.3/post-install.sh
@@ -5,3 +5,6 @@ for tool in php$v php-cgi$v php-config$v phpize$v; do
     sudo update-alternatives --set $tool_name /usr/bin/"$tool_name$v"
   fi
 done
+ini_file=$(php -d "date.timezone=UTC" --ini | grep "Loaded Configuration" | sed -e "s|.*:s*||" | sed "s/ //g")
+sudo chmod 777 "$ini_file"
+echo "date.timezone=UTC" >>"$ini_file"
--- php-5.5/post-install.sh
+++ php-5.5/post-install.sh
@@ -5,5 +5,8 @@ for tool in php$v php-cgi$v php-config$v phpize$v; do
     sudo update-alternatives --set $tool_name /usr/bin/"$tool_name$v"
   fi
 done
+ini_file=$(php -d "date.timezone=UTC" --ini | grep "Loaded Configuration" | sed -e "s|.*:s*||" | sed "s/ //g")
+sudo chmod 777 "$ini_file"
+echo "date.timezone=UTC" >>"$ini_file"
 sudo php5enmod redis
 sudo php5enmod xdebug
