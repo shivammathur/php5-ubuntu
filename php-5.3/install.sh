ce9ff495002f79ad9d2d949f884ad5a202b3d24f
Shivam Mathur <shivam_jpr@hotmail.com>
2020-02-09 21:45:03 +0530

    Add sources.list and fix cURL

--- php-5.3/install.sh
+++ php-5.3/install.sh
@@ -1,8 +1,6 @@
-apt_install="sudo DEBIAN_FRONTEND=noninteractive apt-get install -o Dpkg::Options::=--force-conflicts -y --allow-downgrades --no-upgrade"
 dpkg_install="sudo DEBIAN_FRONTEND=noninteractive dpkg -i --force-conflicts"
 sudo mkdir -p /var/run /run/php
 $dpkg_install ./deps/*.deb
-$apt_install libcurl3
 $dpkg_install ./*.deb
 for tool in php5 php5-cgi php-config5 phpize5; do
   if [ -f /usr/bin/"$tool" ]; then
--- php-5.4/post-install.sh
+++ php-5.4/post-install.sh
@@ -1,4 +1,5 @@
 v=5.4
+dotdeb=http://packages.dotdeb.org
 for tool in php$v php-cgi$v php-config$v phpize$v; do
   if [ -f /usr/bin/"$tool" ]; then
     tool_name=${tool/[0-9]*/}
@@ -8,5 +9,11 @@ done
 ini_file=$(php -d "date.timezone=UTC" --ini | grep "Loaded Configuration" | sed -e "s|.*:s*||" | sed "s/ //g")
 sudo chmod 777 "$ini_file"
 echo "date.timezone=UTC" >>"$ini_file"
+sudo mv ./deps/libcurl.so.3 /usr/lib/libcurl.so.3
+sudo mv ./deps/curl.so "$(php -i | grep "extension_dir => /usr" | sed -e "s|.*=> s*||")"/curl.so
+echo "extension=curl.so" >>"$ini_file"
 sudo php5enmod redis
 sudo php5enmod xdebug
+echo "deb $dotdeb wheezy all" | sudo tee /etc/apt/sources.list.d/dotdeb-ubuntu-php-"$(lsb_release -s -c)".list
+curl -sSLO https://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
