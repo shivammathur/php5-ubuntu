14cda2c62adee72e3908d08b0fd5c0fc40f3596d
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-04 19:55:05 +0530

    Fix apache setup

--- php-5.5/install.sh
+++ php-5.5/install.sh
@@ -1,4 +1,9 @@
-dpkg_install="sudo DEBIAN_FRONTEND=noninteractive dpkg -i --force-conflicts"
+debconf_fix="DEBIAN_FRONTEND=noninteractive"
+dpkg_install="sudo $debconf_fix dpkg -i --force-conflicts --force-conflicts"
+if command -v apache2 && [ "2.2" != "$(apache2 -v 2>/dev/null | grep -Eo "([0-9]+\.[0-9]+)")" ]; then
+  sudo "$debconf_fix" apt-get purge apache2-data apache2-bin apache2 apache2-utils
+  sudo rm -rf /etc/apache2 /var/lib/apache2 /var/lib/apache2 /usr/lib/apache2/modules/* /usr/share/apache2/*
+fi
 sudo mkdir -p /var/run /run/php /usr/lib/php5 /var/www/html
 . /etc/os-release
 $dpkg_install ./deps/"$VERSION_ID"/*.deb
