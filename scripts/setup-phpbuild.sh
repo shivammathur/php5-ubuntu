aa66d24abce9f3eae0edf36b547c076efe944d43
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-21 08:48:16 +0530

    Enable opcache in opcache.ini instead of php.ini in PHP 5.5
    Fix jobs requirements in CI workflow

--- scripts/setup-phpbuild.sh
+++ scripts/setup-phpbuild.sh
@@ -3,3 +3,6 @@
 cp -a php-build/definitions/* /usr/local/share/php-build/definitions/
 cp -a php-build/patches/*.patch /usr/local/share/php-build/patches/
 cp /usr/local/share/php-build/default_configure_options /usr/local/share/php-build/default_configure_options.bak
+
+# Patch to enable opcache in opcache.ini instead of php.ini
+sed -i "s|opcache.so\" >> \"\$PREFIX/etc/php.ini|opcache.so\" >> \"\$PREFIX/etc/conf.d/opcache.ini|" "$(command -v php-build)"
