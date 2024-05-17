2c59c30c8628113c00f8a9be85b3ca87163457b3
Shivam Mathur <shivam_jpr@hotmail.com>
2024-05-17 09:57:13 +0000

    Add ubuntu 24.04
    Update odbc libs for ubuntu 22.04

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -2,7 +2,7 @@
 debconf_fix="DEBIAN_FRONTEND=noninteractive"
 dpkg_install="sudo $debconf_fix dpkg -i --force-conflicts --force-overwrite"
 sudo mkdir -p /var/run /run/php /usr/local/php /usr/lib/systemd/system /usr/lib/cgi-bin /var/www/html
-[[ "$VERSION_ID" = "20.04" || "$VERSION_ID" = "22.04" ]] && $dpkg_install ./deps/"$VERSION_ID"/multiarch-support_2.28-10_amd64
+[[ "$VERSION_ID" = "20.04" || "$VERSION_ID" = "22.04" || $VERSION_ID = "24.04" ]] && $dpkg_install ./deps/"$VERSION_ID"/multiarch-support_2.28-10_amd64
 $dpkg_install ./deps/"$VERSION_ID"/*.deb
 $dpkg_install ./deps/all/*.deb
 sudo tar -I zstd -xf ./php-@PHP_VERSION@-build.tar.zst -C /usr/local/php
